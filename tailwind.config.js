0831757dade1fa6f849115613b8a0504502990f1
Miguel Alejandro <miguel.hernandez@cheapc.com.mx>
2022-02-16 05:24:15 -0600

    first section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
+const { teal } = require('tailwindcss/colors');
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: colors.teal,
+        emerald: colors.emerald,
+        cyan: colors.cyan,
+        rose: colors.rose,
+        pink: colors.pink,
+        sky: colors.sky,
+        blue: colors.blue,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuchsia: colors.fuchsia,
+        bgcolor: '#001127'
+      },
+      boxShadow: {
+        violet: '0px 15px 25px -5px rgba(124, 58, 237, 0.6)',
+        cyan: '0px 15px 25px -5px rgba(34, 211, 238, 0.3)'
+      }
+    },
   },
   plugins: [],
 }
