0fb977e089e0d9a98bf1ea0e1257f7f279d348c4
Miguel <miguel.camarena2004@gmail.com>
2022-02-24 01:15:01 -0600

    finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,16 @@ module.exports = {
       boxShadow: {
         violet: '0px 15px 25px -5px rgba(124, 58, 237, 0.6)',
         cyan: '0px 15px 25px -5px rgba(34, 211, 238, 0.3)',
-        violetxl: '0px 0px 40px 5px rgba(124, 58, 237, 0.6)'
+        violetxl: '0px 0px 50px 10px rgba(124, 58, 237, 0.6)',
+        cyanxl: '0px 0px 50px 10px rgba(34, 211, 238, 0.3)',
+        indigoxl: '0px 0px 50px 10px rgba(79, 70, 229, 0.6)',
+        bluexl: '0px 0px 50px 10px rgba(37, 99, 235, 0.5)',
+        fuchsiaxl: '0px 0px 50px 10px rgba(192, 38, 211, 0.5)',
+        skyxl: '0px 0px 50px 10px rgba(2, 132, 199, 0.5)'
+      },
+      dropShadow: {
+        '3xl': '0 0 50px rgba(192, 38, 211, 0.5)',
+        '3xlteal': '0 0 50px rgba(13, 148, 136, 0.7)'
       }
     },
   },
